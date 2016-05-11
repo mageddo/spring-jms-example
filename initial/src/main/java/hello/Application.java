482742af8d5659fdb4ced8c6d3b7513e4d0bb620
Elvis de Freitas Souza <edigitalb@gmail.com>
2016-05-11 19:13:27 -0300

    creating controller to call receiver

--- initial/src/main/java/hello/Application.java
+++ initial/src/main/java/hello/Application.java
@@ -2,19 +2,14 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
-import org.springframework.jms.core.JmsTemplate;
-import org.springframework.jms.core.MessageCreator;
 import org.springframework.util.FileSystemUtils;
 
 import javax.jms.ConnectionFactory;
 import java.io.File;
-
-import static hello.utils.ReceiversConstants.MAILBOX_DESTINATION;
 
 @SpringBootApplication
 @EnableJms
@@ -31,17 +26,8 @@ public class Application {
     public static void main(String[] args) {
         // Clean out any ActiveMQ data from a previous run
         FileSystemUtils.deleteRecursively(new File("activemq-data"));
-
-        // Launch the application
-        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-
-        // Send a message
-        MessageCreator messageCreator = (session) -> {
-            return session.createTextMessage("ping!");
-        };
-        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
-        System.out.println("Sending a new message.");
-        jmsTemplate.send(MAILBOX_DESTINATION, messageCreator);
+        SpringApplication.run(Application.class, args);
+        System.out.println("system is up!!");
     }
 
 }
