b5858857095f5c471a3dec3f56475c0c70d0536a
Elvis de Freitas Souza <edigitalb@gmail.com>
2016-05-11 20:06:13 -0300

    adding queue id

--- initial/src/main/java/hello/jms/ping/PingReceiver.java
+++ initial/src/main/java/hello/jms/ping/PingReceiver.java
@@ -10,6 +10,8 @@ import static hello.utils.ReceiversConstants.MAILBOX_DESTINATION;
 @Component
 public class PingReceiver {
 
+    private static Integer ID = 0;
+
     /**
      * When you receive a message, print it out, then shut down the application. Finally, clean up any ActiveMQ server
      * stuff.
@@ -17,13 +19,15 @@ public class PingReceiver {
     @JmsListener(destination = MAILBOX_DESTINATION, containerFactory = "myJmsContainerFactory")
     public void receiveMessage(String message) {
 
+        ++ID;
+
         final int sleepTime = new Random().nextInt(1500);
-        System.out.println(String.format("PingReceiver sleeping %d ms....", sleepTime));
+        System.out.println(String.format("PingReceiver %d sleeping %d ms....", ID, sleepTime));
         try {
             Thread.sleep(sleepTime);
         } catch (InterruptedException e) {
         }
-        System.out.println("back to work!");
+        System.out.println(ID + " back to work!");
 
         System.out.println("the receive read the ping message <" + message + ">");
     }
