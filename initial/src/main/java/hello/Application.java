d2ccefef25f03d0a0a4690cc70070735b9d6f7d3
Elvis de Freitas Souza <edigitalb@gmail.com>
2016-05-11 18:51:38 -0300

    creating receiver and app

---
+++ initial/src/main/java/hello/Application.java
@@ -0,0 +1,45 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.JmsListenerContainerFactory;
+import org.springframework.jms.config.SimpleJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
+import org.springframework.util.FileSystemUtils;
+
+import javax.jms.ConnectionFactory;
+import java.io.File;
+
+@SpringBootApplication
+@EnableJms
+public class Application {
+
+    // Strictly speaking this bean is not necessary as boot creates a default
+    @Bean
+    JmsListenerContainerFactory<?> myJmsContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleJmsListenerContainerFactory factory = new SimpleJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        return factory;
+    }
+
+    public static void main(String[] args) {
+        // Clean out any ActiveMQ data from a previous run
+        FileSystemUtils.deleteRecursively(new File("activemq-data"));
+
+        // Launch the application
+        ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+
+        // Send a message
+        MessageCreator messageCreator = (session) -> {
+            return session.createTextMessage("ping!");
+        };
+        JmsTemplate jmsTemplate = context.getBean(JmsTemplate.class);
+        System.out.println("Sending a new message.");
+        jmsTemplate.send("mailbox-destination", messageCreator);
+    }
+
+}
