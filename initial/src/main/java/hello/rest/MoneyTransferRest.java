b023f5e32914445f624a19c0e822d367deb13b84
Elvis <edigitalb@gmail.com>
2016-05-11 23:40:50 -0300

    creating rest service

---
+++ initial/src/main/java/hello/rest/MoneyTransferRest.java
@@ -0,0 +1,25 @@
+package hello.rest;
+
+import hello.bean.MoneyTransaction;
+import hello.jms.MoneyTransferProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by elvis on 11/05/16.
+ */
+@RestController
+public class MoneyTransferRest {
+
+	@Autowired
+	private MoneyTransferProducer moneyTransferProducer;
+
+	@RequestMapping(value = "/user/{name}/transfer", method = RequestMethod.POST)
+	public @ResponseBody void doTransfer(final @RequestBody MoneyTransaction moneyTransaction,
+			 final @PathVariable String senderName){
+
+		moneyTransaction.setSender(senderName);
+		moneyTransferProducer.doTransfer(moneyTransaction);
+
+	}
+}
